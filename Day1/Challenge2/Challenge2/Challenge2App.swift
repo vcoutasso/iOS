2ae5b9528d3011c68c7964aea794c7982eb34f70
Vinícius Couto <vcoutasso@gmail.com>
2021-05-27 18:38:57 -0300

    Day 1 Challenge 2

---
+++ Day1/Challenge2/Challenge2/Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by Vinícius Couto on 27/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
